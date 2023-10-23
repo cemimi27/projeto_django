fdd2ce337cbe05b2484b626c593056073ba79edd
cemimi27 <carloseduardomimi@gmail.com>
2023-10-23 17:55:48 -0300

    Atualização

--- produto/urls.py
+++ produto/urls.py
@@ -5,5 +5,6 @@ app_name = 'produto'
 
 urlpatterns = [
     path('produtos/', views.produtos, name='produtos'),
+    path('detalhes/<int:cod_barras>/', views.detalhesProduto, name='detalhes'),
     path('cadastro/', views.cadastroProduto, name='cadastro'),
 ]
--- produto/views.py
+++ produto/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render, HttpResponse
 from .forms import FormularioProduto
 from .models import Produto
+from django.db.models.functions import Lower
 
 def produtos(request):
-    produtos = Produto.objects.all()
+    produtos = Produto.objects.order_by(Lower("nome_produto"))
     context = {'produtos': produtos}
     return render(request, 'produtos.html', context=context)
 
+def detalhesProduto(request, cod_barras):
+    produto = Produto.objects.get(cod_barras=cod_barras)
+    context = {'produto': produto}
+
+    return render(request, 'produtodetalhe.html', context=context)
+
 def cadastroProduto(request):
+    formulario = FormularioProduto
+    context = {'formulario': formulario, 'msg': ''}
     if request.method == 'POST':
         nome_produto = request.POST.get('nome_produto')
         cod_barras = request.POST.get('cod_barras')
@@ -16,16 +25,22 @@ def cadastroProduto(request):
         descricao = request.POST.get('descricao')
         #context = {'Nome': nome_produto}
         #return render(request, 'cadastroproduto.html', context=context)
-        return validarCadastroProdutoModel(request, nome_produto, cod_barras, preco_produto, qtd_produto, descricao)
+        msg = validarCadastroProdutoModel(request, nome_produto, cod_barras, preco_produto, qtd_produto, descricao)
+        context['msg'] = msg
+        return render(request, 'cadastroproduto.html', context=context)
     else:
-        formulario = FormularioProduto
-        context = {'formulario': formulario}
         return render(request, 'cadastroproduto.html', context=context)
 
 def validarCadastroProdutoModel(request, nome_produto, cod_barras, preco_produto, qtd_produto, descricao):
+    msg = dict()
     produto = Produto.objects.filter(nome_produto=nome_produto)
     if produto:
-        return HttpResponse('Achei')
+        msg['msg'] = 'Produto já cadastrado no sistema!'
+        return msg
     else:
-        return HttpResponse('Não achei')
+        produto = Produto(nome_produto=nome_produto, cod_barras=cod_barras, preco_produto=preco_produto,
+                           qtd_produto=qtd_produto, descricao=descricao)
+        produto.save()
+        msg['msg'] = 'Produto cadastrado com sucesso!'
+        return msg
 
